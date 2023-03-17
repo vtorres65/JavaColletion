be8a896d7dfaf6f5273dba641eaaea3d17d8745f
vtorres65 <vlacky810@gmail.com>
2023-03-17 11:53:00 -0500

    Ejemplos de maps y iteradors

---
+++ src/co/com/alura/Ejemplo15.java
@@ -0,0 +1,36 @@
+package co.com.alura;
+
+import co.com.alura.model.Alumno;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class Ejemplo15 {
+
+    // TODO: https://docs.oracle.com/javase/tutorial/collections/interfaces/index.html
+    // TODO: https://docs.oracle.com/javase/8/docs/api/index.html?java/util/Collection.html
+
+    public static void main(String[] args) {
+        Alumno alumno1 = new Alumno("Luis Miguel", "001") ;
+        Alumno alumno2 = new Alumno("Pepito los palotes", "002") ;
+        Alumno alumno3 = new Alumno("Juan Carlos", "003");
+        Alumno alumno4 = new Alumno("Pedro Pedrito", "004");
+        Alumno alumno5 = new Alumno("Gustavo Sanchez", "005");
+        Alumno alumno6 = new Alumno("Marcia Maria", "006");
+
+        Collection<Alumno> listaAlumnos = new HashSet<>();
+
+        listaAlumnos.add(alumno1);
+        listaAlumnos.add(alumno2);
+        listaAlumnos.add(alumno3);
+        listaAlumnos.add(alumno4);
+        listaAlumnos.add(alumno5);
+        listaAlumnos.add(alumno6);
+
+        Alumno alumnoNuevo = new Alumno("Luis Miguel", "001");
+
+        System.out.println(alumno1.equals(alumnoNuevo));
+        System.out.println(listaAlumnos.contains(alumnoNuevo));
+
+    }
+}
--- src/co/com/alura/model/Curso.java
+++ src/co/com/alura/model/Curso.java
@@ -1,15 +1,15 @@
 package co.com.alura.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.PrimitiveIterator;
+import java.util.*;
 
 public class Curso{
 
     private String nombre;
     private int tiempo;
     private List<Clase> claseList = new ArrayList<>();
+    private Collection<Alumno> alumno = new HashSet<>();
+    //private Collection<Alumno> alumno = new LinkedHashSet<>();
+    private Map<String, Alumno> alumnoMap = new HashMap<>();
 
     public Curso(String nombre, int tiempo) {
         this.nombre = nombre;
@@ -25,17 +25,38 @@ public class Curso{
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public int getTiempo() {
         return tiempo;
     }
-
     public void setTiempo(int tiempo) {
         this.tiempo = tiempo;
+    }
+    public List<Clase> getClaseList() {
+        return Collections.unmodifiableList(claseList);
+    }
+    public void setClaseList(List<Clase> claseList) {
+        this.claseList = claseList;
+    }
+    public void addClase(Clase clase){
+        this.claseList.add(clase);
+    }
+    public int compareTo(Curso o) {
+        return this.nombre.compareTo(o.getNombre());
+    }
+    public void addAlura(Alumno alumno) {
+        this.alumno.add(alumno);
+    }
+    public void addAlumno(Alumno alumno1) {
+        this.alumno.add((Alumno) alumno); this.alumnoMap.put(alumno.getCodigo(), alumno);
+    }
+    public Collection<Alumno> getAlumno() {
+        return alumno;
+    }
+    public boolean verificaAlumno(Alumno alumno) {
+        return this.alumno.contains(alumno);
     }
 
     @Override
@@ -43,19 +64,15 @@ public class Curso{
         return  this.nombre;
     }
 
-    public List<Clase> getClaseList() {
-        return Collections.unmodifiableList(claseList);
+    public <E> List getAlumnoMap() {
+        return null;
     }
 
-    public void setClaseList(List<Clase> claseList) {
-        this.claseList = claseList;
+    public Optional<Object> getAlumnos() {
+        return alumnos;
     }
 
-    public void addClase(Clase clase){
-        this.claseList.add(clase);
-    }
-
-    public int compareTo(Curso o) {
-        return this.nombre.compareTo(o.getNombre());
+    public void setAlumnos(Optional<Object> alumnos) {
+        this.alumnos = alumnos;
     }
 }
